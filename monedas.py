92df82d6e3ae543c258332f9e12f928b170b7204
Franco Loto <franco.loto1995@gmail.com>
2022-11-02 16:33:46 -0300

    Monedas terminadas

--- monedas.py
+++ monedas.py
@@ -45,6 +45,12 @@ class Moneda:
         self.caras = eleccion
 
 
+    def __str__(self):
+        
+        if self.valor_original >= 1:
+            return f"Moneda {int(self.valor_original)} Libra"
+        else:
+            return f"Moneda {int(self.valor_original * 100)} Penique"
 
 class Libra(Moneda):
 
@@ -60,12 +66,68 @@ class Libra(Moneda):
             "peso": 9.5
         }
         super().__init__(**data)
+
+
+class Un_Penique(Moneda):
     
+    def __init__(self):
+
+        data = {
+            "valor_original": 0.01,
+            "color_limpio": "Bronce",
+            "color_oxidado": "Marron",
+            "num_bordes": 1,
+            "diametro": 20.5,
+            "grosor": 2.05,
+            "peso": 3.67
+        }
+        super().__init__(**data)
+
+
+class Dos_Penique(Moneda):
+
+    def __init__(self):
+
+        data = {
+            "valor_original": 0.02,
+            "color_limpio": "Bronce",
+            "color_oxidado": "Marron",
+            "num_bordes": 1,
+            "diametro": 25.7,
+            "grosor": 1.65,
+            "peso": 7.62
+        }
+        super().__init__(**data)
+
+
+
+class Cinco_Penique(Moneda):
+
+    def __init__(self):
+
+        data = {
+            "valor_original": 0.05,
+            "color_limpio": "Plata",
+            "color_oxidado": None,
+            "num_bordes": 1,
+            "diametro": 18.0,
+            "grosor": 1.51,
+            "peso": 4.68
+        }
+        super().__init__(**data)
+
+    def oxido(self):
+        self.color = self.color_limpio
         
 
 
-moneda_libra = Libra()
-moneda_libra.oxido()
-print(moneda_libra.color)
+monedas = [Libra(), Un_Penique(), Dos_Penique(), Cinco_Penique()]
 
+for moneda in monedas:
+    argumentos = [moneda, moneda.color, moneda.valor, moneda.diametro,
+                  moneda.grosor, moneda.num_bordes, moneda.peso]
+
+
+cadena = f"{moneda} - Color: {moneda.color}, Diametro: {moneda.diametro}, Grosor: {moneda.grosor}, Valor: {moneda.valor}, Bordes: {moneda.num_bordes}, Peso: {moneda.peso}"
+print(cadena)
 
